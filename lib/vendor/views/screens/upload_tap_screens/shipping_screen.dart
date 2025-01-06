8d616797bf0b46fa412375d6b118d97910397b25
Medagangoda <140582352+Medagangoda@users.noreply.github.com>
2025-01-07 02:51:40 +0530

    state management/ Shipping Charge

--- lib/vendor/views/screens/upload_tap_screens/shipping_screen.dart
+++ lib/vendor/views/screens/upload_tap_screens/shipping_screen.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:multi_store_mobile/provider/product_provider.dart';
+import 'package:provider/provider.dart';
 
-class ShippingScreen extends StatelessWidget {
-  const ShippingScreen({super.key});
+class ShippingScreen extends StatefulWidget {
+  @override
+  State<ShippingScreen> createState() => _ShippingScreenState();
+}
+
+class _ShippingScreenState extends State<ShippingScreen> {
+  bool? _chargeShipping = false;
+
+
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Shipping Screen'),
+    final ProductProvider _productProvider = Provider.of<ProductProvider>(context);
+    return Column(
+      children: [
+        CheckboxListTile(
+          title: Text(
+            'Change Shipping',
+            style: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, letterSpacing: 4),
+          ),
+          value: _chargeShipping,
+          onChanged: (value) {
+            setState(() {
+              _chargeShipping = value;
+              _productProvider.getFromData(chargeShipping: _chargeShipping);
+            });
+          },
+        ),
+        if (_chargeShipping == true)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              onChanged: (value) {
+                
+                _productProvider.getFromData(shippingCharge: int.parse(value));
+              },
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Shipping Change'),
+            ),
+          )
+      ],
     );
   }
 }
