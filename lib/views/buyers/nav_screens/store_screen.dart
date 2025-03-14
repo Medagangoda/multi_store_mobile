45ae7997a2c25b643fb737772857495b36d1db01
Medagangoda <140582352+Medagangoda@users.noreply.github.com>
2025-03-14 10:19:15 +0530

    store screen build

--- lib/views/buyers/nav_screens/store_screen.dart
+++ lib/views/buyers/nav_screens/store_screen.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class StoreScreen extends StatelessWidget {
@@ -7,8 +8,38 @@ class StoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('store screen'),
+      final Stream<QuerySnapshot> _vendorsStream = FirebaseFirestore.instance.collection('vendors').snapshots();
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: _vendorsStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return Container(
+          height: 500,
+          child: ListView.builder(
+            itemCount: snapshot.data!.size,
+            itemBuilder: (context, index) {
+              final storeData = snapshot.data!.docs[index];
+              return ListTile(
+                title: Text(storeData['bussinessName']),
+                subtitle: Text(storeData['countryValue']),
+
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    storeData['storeImage']
+                  ),
+                ),
+              );
+            }),
+        );
+      },
     );
   }
 }
