214607676556a80c601cc57b05d0f22e70a18c2c
Medagangoda <140582352+Medagangoda@users.noreply.github.com>
2025-03-17 02:36:57 +0530

    search screen build

--- lib/views/buyers/nav_screens/search_screen.dart
+++ lib/views/buyers/nav_screens/search_screen.dart
@@ -1,16 +1,99 @@
-
-
-
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget {
-  const SearchScreen({super.key});
+class SearchScreen extends StatefulWidget {
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  String _searchedValue = '';
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('search screen'),
+    final Stream<QuerySnapshot> _productsStream =
+        FirebaseFirestore.instance.collection('products').snapshots();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.yellow.shade800,
+        elevation: 0,
+        title: TextFormField(
+          onChanged: (value) {
+            setState(() {
+              _searchedValue = value;
+            });
+          },
+          decoration: InputDecoration(
+              labelText: 'Search For Product',
+              labelStyle: TextStyle(
+                color: Colors.white,
+                letterSpacing: 4,
+              ),
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.white,
+              )),
+        ),
+      ),
+      body: _searchedValue == ''? Center(
+        child: Text(
+          'Search For Products',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 5,
+          ),
+        ),
+      ): StreamBuilder<QuerySnapshot>(
+        stream: _productsStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          }
+
+          final searchedData = snapshot.data!.docs.where((element) {
+            return element['productName']
+                .toLowerCase()
+                .contains(_searchedValue.toLowerCase());
+          });
+
+          return Column(
+            children: searchedData.map((e) {
+              return Card(
+                child: Row(
+                  children: [
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Image.network(e['imageUrl'][0]),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          e['productName'],
+                          style: TextStyle(
+                              fontSize: 10, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          e['productPrice'].toStringAsFixed(2),
+                          style: TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.yellow.shade700),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          );
+        },
+      ),
     );
   }
 }
