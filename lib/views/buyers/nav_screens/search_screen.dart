afeda1d85fc9cd29cab0bde04ed316d902c7e6a6
Medagangoda <140582352+Medagangoda@users.noreply.github.com>
2025-03-17 03:18:01 +0530

    product search details

--- lib/views/buyers/nav_screens/search_screen.dart
+++ lib/views/buyers/nav_screens/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_store_mobile/views/buyers/productDetail/product_detail_screen.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -35,65 +36,79 @@ class _SearchScreenState extends State<SearchScreen> {
               )),
         ),
       ),
-      body: _searchedValue == ''? Center(
-        child: Text(
-          'Search For Products',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 5,
-          ),
-        ),
-      ): StreamBuilder<QuerySnapshot>(
-        stream: _productsStream,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Something went wrong');
-          }
-
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text("Loading");
-          }
-
-          final searchedData = snapshot.data!.docs.where((element) {
-            return element['productName']
-                .toLowerCase()
-                .contains(_searchedValue.toLowerCase());
-          });
-
-          return Column(
-            children: searchedData.map((e) {
-              return Card(
-                child: Row(
-                  children: [
-                    SizedBox(
-                      height: 100,
-                      width: 100,
-                      child: Image.network(e['imageUrl'][0]),
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          e['productName'],
-                          style: TextStyle(
-                              fontSize: 10, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          e['productPrice'].toStringAsFixed(2),
-                          style: TextStyle(
-                              fontSize: 10,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.yellow.shade700),
-                        ),
-                      ],
-                    ),
-                  ],
+      body: _searchedValue == ''
+          ? Center(
+              child: Text(
+                'Search For Products',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 5,
                 ),
-              );
-            }).toList(),
-          );
-        },
-      ),
+              ),
+            )
+          : StreamBuilder<QuerySnapshot>(
+              stream: _productsStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Something went wrong');
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Text("Loading");
+                }
+
+                final searchedData = snapshot.data!.docs.where((element) {
+                  return element['productName']
+                      .toLowerCase()
+                      .contains(_searchedValue.toLowerCase());
+                });
+
+                return Column(
+                  children: searchedData.map((e) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return ProductDetailScreen(
+                            productData: e,
+                          );
+                        }));
+                      },
+                      child: Card(
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              height: 100,
+                              width: 100,
+                              child: Image.network(e['imageUrl'][0]),
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  e['productName'],
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  e['productPrice'].toStringAsFixed(2),
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.yellow.shade700),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                );
+              },
+            ),
     );
   }
 }
